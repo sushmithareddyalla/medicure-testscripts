507df1e29a99f1045a1adf014b79900b65e45527
sushmithareddyalla <147145664+sushmithareddyalla@users.noreply.github.com>
2023-11-29 08:32:31 -0800

    Update App.java

--- src/main/java/medicure/testscripts/App.java
+++ src/main/java/medicure/testscripts/App.java
@@ -19,10 +19,10 @@ public class App
     {
     System.out.println("Script Started");	
        //initializing the web driver
-    System.setProperty("webdriver.chrome.driver", "C:\\Users\\dell\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+    //System.setProperty("webdriver.chrome.driver", "C:\\Users\\dell\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
     //setting properties
     ChromeOptions chromeOptions = new ChromeOptions();
-	//chromeOptions.addArguments("--headless");
+	chromeOptions.addArguments("--headless");
 	//chromeOptions.addArguments("--disable-gpu");
 	//chromeOptions.addArguments("--no-sandbox");
     //open url
